fc9efa0ff96b51e68ac578a27c814f813728becf
SiranTang <695746340@qq.com>
2019-06-22 10:00:08 +0800

    login

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import UserLayout from '../../layouts/UserLayout';
-import {Form, Icon, Input, Card} from 'antd';
+import {Form, Icon, Input, Card,Button,notification,} from 'antd';
 import "./style.scss"
+import createHistory from 'history/createHashHistory'
+
+const FormItem = Form.Item
+const history = createHistory()
 
 class NormalLoginForm extends React.Component {
     handleSubmit(){
-
+        this.openNotificationWithIcon('info')
     }
+    
+
     render(){
         const { getFieldDecorator } = this.props.form;
         return (
             <UserLayout>
+                <div className="loginpagewrap">
+                    <div className="box">
                 <header className="header">区块链供应链金融平台</header>
-                <Card>
+                    <div className="loginWrap">
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
                         {getFieldDecorator('username', {
@@ -35,8 +43,13 @@ class NormalLoginForm extends React.Component {
                             />,
                         )}
                         </Form.Item>
+                        <Button type="primary" htmlType="submit" className="loginBtn">
+                            Login
+                        </Button>
                     </Form>
-                </Card>
+                    </div>
+                    </div>
+                </div>
             </UserLayout>
         )
     }
